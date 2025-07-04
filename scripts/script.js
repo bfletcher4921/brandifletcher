1ebf0ff535c387cad3a8ce9133d719b8b15efed1
bfletcher4921 <bfletcher1@una.edu>
2025-07-04 08:35:35 -0500

    commit help session changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,40 @@
-//comment
+// doc ready f/n
+document.addEventListener("DOMContentLoaded", () => {
+  console.log ('hello console!')
+  setCookie(); 
+  greetCookieUser(); 
+});
 
-console.log ('hello console!')
+// cookie 
+function setCookie() {
+  // const d = new Date();
+  // d.setTime(d.getTime() + (7*24*60*60*1000));
+  // const expires = "expires=" + d.toUTCString();
+  // const userName = encodeURIComponent("John Doe");
+  document.cookie = `exampleCookie=Barry; path=/`;
+  console.log("Cookie set:", document.cookie);
+}
+
+function getUserNameFromCookie() {
+  const match = document.cookie.match(/exampleCookie=([^;]+)/);
+  if (match) {
+    const decoded = decodeURIComponent(match[1]);
+    console.log("User name from cookie:", decoded);
+    return decoded;
+  }
+  console.log("User name not found in cookie.");
+  return null;
+}
+
+function greetCookieUser() {
+  //get the cookie
+  const userName = getUserNameFromCookie();
+  console.log("user name:", userName)
+  //say heloo 
+  document.getElementById("userGreeting").innerText = "Hello, " + userName + "!";
+}
+
+
+
+
+//comment
