2081ef2106cdb39a78f703767efbe9aca52b9fd2
bfletcher4921 <bfletcher1@una.edu>
2025-07-11 18:38:18 -0500

    add improvements to dev profile

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded", () => {
   setCookie(); 
   greetCookieUser(); 
 });
-<<<<<<< HEAD
 
 // cookie 
 function setCookie() {
@@ -12,42 +11,7 @@ function setCookie() {
   // d.setTime(d.getTime() + (7*24*60*60*1000));
   // const expires = "expires=" + d.toUTCString();
   // const userName = encodeURIComponent("John Doe");
-  document.cookie = `exampleCookie=Barry; path=/`;
-  console.log("Cookie set:", document.cookie);
-}
-
-function getUserNameFromCookie() {
-  const match = document.cookie.match(/exampleCookie=([^;]+)/);
-  if (match) {
-    const decoded = decodeURIComponent(match[1]);
-    console.log("User name from cookie:", decoded);
-    return decoded;
-  }
-  console.log("User name not found in cookie.");
-  return null;
-}
-
-function greetCookieUser() {
-  //get the cookie
-  const userName = getUserNameFromCookie();
-  console.log("user name:", userName)
-  //say heloo 
-  document.getElementById("userGreeting").innerText = "Hello, " + userName + "!";
-}
-
-
-
-
-//comment
-=======
-
-// cookie 
-function setCookie() {
-  // const d = new Date();
-  // d.setTime(d.getTime() + (7*24*60*60*1000));
-  // const expires = "expires=" + d.toUTCString();
-  // const userName = encodeURIComponent("John Doe");
-  document.cookie = `exampleCookie=Barry; path=/`;
+  document.cookie = `exampleCookie=Welcome Visitor; path=/`;
   console.log("Cookie set:", document.cookie);
 }
 
@@ -75,4 +39,3 @@ function greetCookieUser() {
 
 //comment
 
->>>>>>> 182e80aa0f6e4e937c78e21580d3ce888fb38122
